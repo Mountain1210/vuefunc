f217eef62ab088d24623143ff210faa131b7d480
Mountain1210 <mountain_wang@126.com>
2017-12-01 22:25:29 +0800

    滑动门效果

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <Tabs/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Tabs from './components/Tabs'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Tabs
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/tabs.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+        <ul>
+            <li 
+            v-for="(item,index) in tabs" 
+            :class="{active:index == num}"
+             @click="tab(index)">{{item}}</li>
+        </ul>
+        <div class="tabCon">
+            <div 
+            v-for='(itemCon,index) in tabContents' 
+            v-show=" index == num">{{itemCon}}</div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabs',
+ data() {
+ return{
+         tabs: ["标题一", "标题二","标题三"],
+         tabContents: ["内容一", "内容二","内容三"],
+         num: 1}
+    },
+    methods: {
+        tab(index) {
+            this.num = index;
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
